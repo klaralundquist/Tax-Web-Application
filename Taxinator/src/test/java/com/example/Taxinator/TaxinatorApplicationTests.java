8f3d8b5e1d53dbc186dde90fd58cd48ec7dbcb13
WeiJon <111754370+WeiJon@users.noreply.github.com>
2022-09-07 09:19:47 +0200

    Add files via upload

---
+++ Taxinator/src/test/java/com/example/Taxinator/TaxinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Taxinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
