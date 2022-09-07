3e711dd98cdabf128b9cc477aa2f69dce225ded3
SebastianBarufke <sebastian.barufke@awacademy.training>
2022-09-07 11:17:47 +0200

    Created calculator method

--- Taxinator/src/main/java/com/example/Taxinator/KommunRepository.java
+++ Taxinator/src/main/java/com/example/Taxinator/KommunRepository.java
@@ -1,9 +1,22 @@
 package com.example.Taxinator;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+
+@Repository
 public class KommunRepository {
       private List<Kommun> kommuner;
 
 
+            public Double calculator(Kommun kommun, Double userInput){
+              Double result;
+              Double churchTax=0.0025;
+
+              if(kommun.isChurchMember){result=(userInput*churchTax)*kommun.getTaxRate();
+
+              }result=userInput*kommun.getTaxRate();
+            return result;}
 }
+
--- Taxinator/src/main/java/com/example/Taxinator/TaxinatorController.java
+++ Taxinator/src/main/java/com/example/Taxinator/TaxinatorController.java
@@ -1,8 +1,11 @@
 package com.example.Taxinator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 public class TaxinatorController {
