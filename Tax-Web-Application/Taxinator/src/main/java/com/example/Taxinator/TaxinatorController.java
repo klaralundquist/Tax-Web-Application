7b0e4e025ac38abee13d078e875fe2947ee25462
KlaraLundquist <klaralundqvist@awacademy.training>
2022-09-08 10:55:25 +0200

    Added percentage rate to html and controller for kommun

--- Tax-Web-Application/Taxinator/src/main/java/com/example/Taxinator/TaxinatorController.java
+++ Tax-Web-Application/Taxinator/src/main/java/com/example/Taxinator/TaxinatorController.java
@@ -1,11 +1,14 @@
 package com.example.Taxinator;
 
+import org.apache.commons.math3.util.Precision;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.math.BigDecimal;
 
 @Controller
 public class TaxinatorController {
@@ -25,8 +28,16 @@ public class TaxinatorController {
         repository.applyTax(kommun);
         model.addAttribute("salaryAfterTax", repository.calculator(kommun));
 
+        BigDecimal bigDecimal = kommun.getTaxRate();
+        Double taxRate = bigDecimal.doubleValue();
+        Double percentageOfTax = (1-taxRate) * 100;
+        //BigDecimal percentage = BigDecimal.valueOf(percentageOfTax);
+        BigDecimal test = BigDecimal.valueOf(Precision.round(percentageOfTax, 2));
+
+        model.addAttribute("percentage", test);
         model.addAttribute("kommuner", repository.getKommuner());
         return "home";
     }
+
 
 }
